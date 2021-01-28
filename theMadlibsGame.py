3bfe57b92cee024c5a355783cad53494bf0c7869
Mansi-Sen <70091467+Mansi-Sen@users.noreply.github.com>
2021-01-28 19:05:56 +0530

    Initial commit

---
+++ theMadlibsGame.py
@@ -0,0 +1,19 @@
+a = input("adjective:")
+b = input("noun:")
+c = input("verb(past tense):")
+d = input("adverb:")
+e = input("adjective:")
+f = input("noun:")
+g = input("noun:")
+h = input("adjective:")
+i = input("verb:")
+j = input("adverb:")
+k = input("verb:")
+l = input("adjective:")
+
+
+
+
+madlib =f"Today I went to the zoo. I saw a(n){a} {b} jumping up and down in its tree.He {c} {d} through the large tunnel that led to its {e} {f}. I got some peanuts and passedthem through the cage to a gigantic gray {g} towering above my head. Feeding that animal mademe hungry. I went to get a {h} scoopof ice cream. It filled my stomach. Afterwards I had to {i} {j} to catch our bus.When I got home I {k} mymom for a {l} day at the zoo."
+
+print(madlib)
